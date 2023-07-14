75ac7ae36aa615ee939a9f14577b504bb3398360
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2023-07-14 13:58:39 +0800

    Update

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/owe.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/owe.java
@@ -11,6 +11,7 @@ public abstract class owe extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_owe);
       Toast.makeText((Context)owe.this, (CharSequence)"#欢迎使用Beta版!!!", Toast.LENGTH_LONG).show();
     }
 }
