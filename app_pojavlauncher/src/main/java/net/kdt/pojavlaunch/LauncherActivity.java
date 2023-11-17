8327a6d45b37b4357de4cee32fa8988b42a3d833
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2023-11-17 22:15:21 +0800

    Update

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/LauncherActivity.java
@@ -0,0 +1,327 @@
+package net.kdt.pojavlaunch;
+
+import android.Manifest;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+
+import com.kdt.mcgui.ProgressLayout;
+import com.kdt.mcgui.mcAccountSpinner;
+
+import net.kdt.pojavlaunch.lifecycle.ContextExecutor;
+import net.kdt.pojavlaunch.contracts.OpenDocumentWithExtension;
+import net.kdt.pojavlaunch.extra.ExtraConstants;
+import net.kdt.pojavlaunch.extra.ExtraCore;
+import net.kdt.pojavlaunch.extra.ExtraListener;
+import net.kdt.pojavlaunch.fragments.MainMenuFragment;
+import net.kdt.pojavlaunch.fragments.MicrosoftLoginFragment;
+import net.kdt.pojavlaunch.fragments.SelectAuthFragment;
+import net.kdt.pojavlaunch.modloaders.modpacks.ModloaderInstallTracker;
+import net.kdt.pojavlaunch.modloaders.modpacks.imagecache.IconCacheJanitor;
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
+import net.kdt.pojavlaunch.prefs.screens.LauncherPreferenceFragment;
+import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
+import net.kdt.pojavlaunch.progresskeeper.TaskCountListener;
+import net.kdt.pojavlaunch.services.ProgressServiceKeeper;
+import net.kdt.pojavlaunch.tasks.AsyncMinecraftDownloader;
+import net.kdt.pojavlaunch.tasks.AsyncVersionList;
+import net.kdt.pojavlaunch.lifecycle.ContextAwareDoneListener;
+import net.kdt.pojavlaunch.utils.NotificationUtils;
+import net.kdt.pojavlaunch.value.launcherprofiles.LauncherProfiles;
+import net.kdt.pojavlaunch.value.launcherprofiles.MinecraftProfile;
+
+import java.lang.ref.WeakReference;
+
+public class LauncherActivity extends BaseActivity {
+    public static final String SETTING_FRAGMENT_TAG = "SETTINGS_FRAGMENT";
+
+    public final ActivityResultLauncher<Object> modInstallerLauncher =
+            registerForActivityResult(new OpenDocumentWithExtension("jar"), (data)->{
+                if(data != null) Tools.launchModInstaller(this, data);
+            });
+
+    private mcAccountSpinner mAccountSpinner;
+    private FragmentContainerView mFragmentView;
+    private ImageButton mSettingsButton, mDeleteAccountButton;
+    private ProgressLayout mProgressLayout;
+    private ProgressServiceKeeper mProgressServiceKeeper;
+    private ModloaderInstallTracker mInstallTracker;
+    private NotificationManager mNotificationManager;
+
+    /* Allows to switch from one button "type" to another */
+    private final FragmentManager.FragmentLifecycleCallbacks mFragmentCallbackListener = new FragmentManager.FragmentLifecycleCallbacks() {
+        @Override
+        public void onFragmentResumed(@NonNull FragmentManager fm, @NonNull Fragment f) {
+            mSettingsButton.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), f instanceof MainMenuFragment
+                    ? R.drawable.ic_menu_settings : R.drawable.ic_menu_home));
+        }
+    };
+
+    /* Listener for the back button in settings */
+    private final ExtraListener<String> mBackPreferenceListener = (key, value) -> {
+        if(value.equals("true")) onBackPressed();
+        return false;
+    };
+
+    /* Listener for the auth method selection screen */
+    private final ExtraListener<Boolean> mSelectAuthMethod = (key, value) -> {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(mFragmentView.getId());
+        // Allow starting the add account only from the main menu, should it be moved to fragment itself ?
+        if(!(fragment instanceof MainMenuFragment)) return false;
+
+        Tools.swapFragment(this, SelectAuthFragment.class, SelectAuthFragment.TAG, true, null);
+        return false;
+    };
+
+    /* Listener for the settings fragment */
+    private final View.OnClickListener mSettingButtonListener = v -> {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(mFragmentView.getId());
+        if(fragment instanceof MainMenuFragment){
+            Tools.swapFragment(this, LauncherPreferenceFragment.class, SETTING_FRAGMENT_TAG, true, null);
+        } else{
+            // The setting button doubles as a home button now
+            while(!(getSupportFragmentManager().findFragmentById(mFragmentView.getId()) instanceof MainMenuFragment)){
+                getSupportFragmentManager().popBackStackImmediate();
+            }
+        }
+    };
+
+    /* Listener for account deletion */
+    private final View.OnClickListener mAccountDeleteButtonListener = v -> new AlertDialog.Builder(this)
+            .setMessage(R.string.warning_remove_account)
+            .setPositiveButton(android.R.string.cancel, null)
+            .setNeutralButton(R.string.global_delete, (dialog, which) -> mAccountSpinner.removeCurrentAccount())
+            .show();
+
+    private final ExtraListener<Boolean> mLaunchGameListener = (key, value) -> {
+        if(mProgressLayout.hasProcesses()){
+            Toast.makeText(this, R.string.tasks_ongoing, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        String selectedProfile = LauncherPreferences.DEFAULT_PREF.getString(LauncherPreferences.PREF_KEY_CURRENT_PROFILE,"");
+        if (LauncherProfiles.mainProfileJson == null || !LauncherProfiles.mainProfileJson.profiles.containsKey(selectedProfile)){
+            Toast.makeText(this, R.string.error_no_version, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        MinecraftProfile prof = LauncherProfiles.mainProfileJson.profiles.get(selectedProfile);
+        if (prof == null || prof.lastVersionId == null || "Unknown".equals(prof.lastVersionId)){
+            Toast.makeText(this, R.string.error_no_version, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if(mAccountSpinner.getSelectedAccount() == null){
+            Toast.makeText(this, R.string.no_saved_accounts, Toast.LENGTH_LONG).show();
+            ExtraCore.setValue(ExtraConstants.SELECT_AUTH_METHOD, true);
+            return false;
+        }
+        String normalizedVersionId = AsyncMinecraftDownloader.normalizeVersionId(prof.lastVersionId);
+        JMinecraftVersionList.Version mcVersion = AsyncMinecraftDownloader.getListedVersion(normalizedVersionId);
+        new AsyncMinecraftDownloader().start(
+                this,
+                mcVersion,
+                normalizedVersionId,
+                new ContextAwareDoneListener(this, normalizedVersionId)
+        );
+        return false;
+    };
+
+    private final TaskCountListener mDoubleLaunchPreventionListener = taskCount -> {
+        // Hide the notification that starts the game if there are tasks executing.
+        // Prevents the user from trying to launch the game with tasks ongoing.
+        if(taskCount > 0) {
+            Tools.runOnUiThread(() ->
+                    mNotificationManager.cancel(NotificationUtils.NOTIFICATION_ID_GAME_START)
+            );
+        }
+    };
+
+    private ActivityResultLauncher<String> mRequestNotificationPermissionLauncher;
+    private WeakReference<Runnable> mRequestNotificationPermissionRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pojav_launcher);
+        IconCacheJanitor.runJanitor();
+        mRequestNotificationPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                isAllowed -> {
+                    if(!isAllowed) handleNoNotificationPermission();
+                    else {
+                        Runnable runnable = Tools.getWeakReference(mRequestNotificationPermissionRunnable);
+                        if(runnable != null) runnable.run();
+                    }
+                }
+        );
+        getWindow().setBackgroundDrawable(null);
+        bindViews();
+        checkNotificationPermission();
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        ProgressKeeper.addTaskCountListener(mDoubleLaunchPreventionListener);
+        ProgressKeeper.addTaskCountListener((mProgressServiceKeeper = new ProgressServiceKeeper(this)));
+
+        mSettingsButton.setOnClickListener(mSettingButtonListener);
+        mDeleteAccountButton.setOnClickListener(mAccountDeleteButtonListener);
+        ProgressKeeper.addTaskCountListener(mProgressLayout);
+        ExtraCore.addExtraListener(ExtraConstants.BACK_PREFERENCE, mBackPreferenceListener);
+        ExtraCore.addExtraListener(ExtraConstants.SELECT_AUTH_METHOD, mSelectAuthMethod);
+
+        ExtraCore.addExtraListener(ExtraConstants.LAUNCH_GAME, mLaunchGameListener);
+
+        new AsyncVersionList().getVersionList(versions -> ExtraCore.setValue(ExtraConstants.RELEASE_TABLE, versions), false);
+
+        mInstallTracker = new ModloaderInstallTracker(this);
+
+        mProgressLayout.observe(ProgressLayout.DOWNLOAD_MINECRAFT);
+        mProgressLayout.observe(ProgressLayout.UNPACK_RUNTIME);
+        mProgressLayout.observe(ProgressLayout.INSTALL_MODPACK);
+        mProgressLayout.observe(ProgressLayout.AUTHENTICATE_MICROSOFT);
+        mProgressLayout.observe(ProgressLayout.DOWNLOAD_VERSION_LIST);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ContextExecutor.setActivity(this);
+        mInstallTracker.attach();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ContextExecutor.clearActivity();
+        mInstallTracker.detach();
+    }
+
+    @Override
+    public boolean setFullscreen() {
+        return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getSupportFragmentManager().registerFragmentLifecycleCallbacks(mFragmentCallbackListener, true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mProgressLayout.cleanUpObservers();
+        ProgressKeeper.removeTaskCountListener(mProgressLayout);
+        ProgressKeeper.removeTaskCountListener(mProgressServiceKeeper);
+        ExtraCore.removeExtraListenerFromValue(ExtraConstants.BACK_PREFERENCE, mBackPreferenceListener);
+        ExtraCore.removeExtraListenerFromValue(ExtraConstants.SELECT_AUTH_METHOD, mSelectAuthMethod);
+        ExtraCore.removeExtraListenerFromValue(ExtraConstants.LAUNCH_GAME, mLaunchGameListener);
+
+        getSupportFragmentManager().unregisterFragmentLifecycleCallbacks(mFragmentCallbackListener);
+    }
+
+    /** Custom implementation to feel more natural when a backstack isn't present */
+    @Override
+    public void onBackPressed() {
+        MicrosoftLoginFragment fragment = (MicrosoftLoginFragment) getVisibleFragment(MicrosoftLoginFragment.TAG);
+        if(fragment != null){
+            if(fragment.canGoBack()){
+                fragment.goBack();
+                return;
+            }
+        }
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        LauncherPreferences.computeNotchSize(this);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private Fragment getVisibleFragment(String tag){
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(tag);
+        if(fragment != null && fragment.isVisible()) {
+            return fragment;
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unused")
+    private Fragment getVisibleFragment(int id){
+        Fragment fragment = getSupportFragmentManager().findFragmentById(id);
+        if(fragment != null && fragment.isVisible()) {
+            return fragment;
+        }
+        return null;
+    }
+
+    private void checkNotificationPermission() {
+        if(LauncherPreferences.PREF_SKIP_NOTIFICATION_PERMISSION_CHECK ||
+            checkForNotificationPermission()) {
+            return;
+        }
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS)) {
+            showNotificationPermissionReasoning();
+            return;
+        }
+        askForNotificationPermission(null);
+    }
+
+    private void showNotificationPermissionReasoning() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.notification_permission_dialog_title)
+                .setMessage(R.string.notification_permission_dialog_text)
+                .setPositiveButton(android.R.string.ok, (d, w) -> askForNotificationPermission(null))
+                .setNegativeButton(android.R.string.cancel, (d, w)-> handleNoNotificationPermission())
+                .show();
+    }
+
+    private void handleNoNotificationPermission() {
+        LauncherPreferences.PREF_SKIP_NOTIFICATION_PERMISSION_CHECK = true;
+        LauncherPreferences.DEFAULT_PREF.edit()
+                .putBoolean(LauncherPreferences.PREF_KEY_SKIP_NOTIFICATION_CHECK, true)
+                .apply();
+        Toast.makeText(this, R.string.notification_permission_toast, Toast.LENGTH_LONG).show();
+    }
+
+    public boolean checkForNotificationPermission() {
+        return Build.VERSION.SDK_INT < 33 || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_DENIED;
+    }
+
+    public void askForNotificationPermission(Runnable onSuccessRunnable) {
+        if(Build.VERSION.SDK_INT < 33) return;
+        if(onSuccessRunnable != null) {
+            mRequestNotificationPermissionRunnable = new WeakReference<>(onSuccessRunnable);
+        }
+        mRequestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+    }
+
+    /** Stuff all the view boilerplate here */
+    private void bindViews(){
+        mFragmentView = findViewById(R.id.container_fragment);
+        mSettingsButton = findViewById(R.id.setting_button);
+        mDeleteAccountButton = findViewById(R.id.delete_account_button);
+        mAccountSpinner = findViewById(R.id.account_spinner);
+        mProgressLayout = findViewById(R.id.progress_layout);
+    }
+}
