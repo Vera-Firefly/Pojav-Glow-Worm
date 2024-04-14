61c3121e66c63b98f18158cbf05bb5af7e86f0d7
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-04-14 19:08:09 +0800

    Try auto unpacj jre11

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
@@ -71,6 +71,7 @@ public class PojavApplication extends Application {
 												.concat("/x86");
 			}
 			AsyncAssetManager.unpackRuntime(getAssets());
+			AsyncAssetManager.unpackRuntime11(getAssets());
 		} catch (Throwable throwable) {
 			Intent ferrorIntent = new Intent(this, FatalErrorActivity.class);
 			ferrorIntent.putExtra("throwable", throwable);
