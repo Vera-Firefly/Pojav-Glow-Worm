a549ed2c88f6199f7d0e8b03a8c72995a2111e11
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-04-16 13:33:16 +0800

    Fix

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/mouse/Touchpad.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/mouse/Touchpad.java
@@ -11,6 +11,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.imageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
