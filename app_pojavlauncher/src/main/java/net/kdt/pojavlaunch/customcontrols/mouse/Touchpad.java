61977225ca496a1a13800d54a82f8375d28bfdae
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-04-16 13:44:51 +0800

    …

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/mouse/Touchpad.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/customcontrols/mouse/Touchpad.java
@@ -11,7 +11,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.*;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
