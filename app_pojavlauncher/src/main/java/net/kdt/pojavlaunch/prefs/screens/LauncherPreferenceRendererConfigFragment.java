936938032d2d2cca5e77eea5fc79d7fad061fe37
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-05-31 21:46:37 +0800

    Remove Incorrect Import

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererConfigFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Toast.LENGTH_SHORT;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
