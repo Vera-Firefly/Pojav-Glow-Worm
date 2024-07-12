da57903c95040248ed7700fd82c97649ee226294
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-07-12 11:51:47 +0800

    Feat[launcher]: ranked Java Runtime picking from two sources & New decompression schema

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import net.kdt.pojavlaunch.lifecycle.ContextExecutor;
-import net.kdt.pojavlaunch.tasks.AsyncAssetManager;
 import net.kdt.pojavlaunch.utils.*;
 import net.kdt.pojavlaunch.utils.FileUtils;
 
@@ -73,8 +72,6 @@ public class PojavApplication extends Application {
 												originalJNIDirectory.lastIndexOf("/"))
 												.concat("/x86");
 			}
-			AsyncAssetManager.unpackRuntime(getAssets());
-			AsyncAssetManager.unpackRuntime11(getAssets());
 			UnpackJRE.unpackAllJre(getAssets());
 		} catch (Throwable throwable) {
 			Intent ferrorIntent = new Intent(this, FatalErrorActivity.class);
