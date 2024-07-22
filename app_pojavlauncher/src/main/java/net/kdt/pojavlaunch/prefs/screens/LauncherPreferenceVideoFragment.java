7a958d2de96d21b825da2a69f6ebb179768dc020
Vera-Firefly <1964762970@qq.com>
2024-07-22 12:27:44 +0800

    Feat[Video]: Custom Video Resolution

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
@@ -3,10 +3,21 @@ package net.kdt.pojavlaunch.prefs.screens;
 import static net.kdt.pojavlaunch.prefs.LauncherPreferences.DEFAULT_PREF;
 import static net.kdt.pojavlaunch.prefs.LauncherPreferences.PREF_NOTCH_SIZE;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.SwitchPreferenceCompat;
@@ -20,6 +31,7 @@ import net.kdt.pojavlaunch.prefs.LauncherPreferences;
  * Fragment for any settings video related
  */
 public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment {
+    private EditText mSetVideoResolution;
 
     @Override
     public void onCreatePreferences(Bundle b, String str) {
@@ -40,6 +52,11 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
         if (seek5.getValue() < 25) {
             seek5.setValue(100);
         }
+
+        seek5.setOnPreferenceClickListener(preference -> {
+            setVideoResolutionDialog(seek5);
+            return true;
+        });
 
         // Sustained performance is only available since Nougat
         SwitchPreference sustainedPerfSwitch = requirePreference("sustainedPerformance",
@@ -77,6 +94,47 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
         }
         listPreference.setEntries(array.getArray());
         listPreference.setEntryValues(array.getList().toArray(new String[0]));
+    }
+
+    private void setVideoResolutionDialog(CustomSeekBarPreference seek) {
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_video_resolution, null);
+        mSetVideoResolution = view.findViewById(R.id.set_resolution);
+        mSetVideoResolution.setText(String.valueOf(seek.getValue()));
+        AlertDialog dialog = new AlertDialog.Builder(requireContext())
+            .setView(view)
+            .setPositiveButton(R.string.alertdialog_done, (dia, i) -> {
+                String checkValue = mSetVideoResolution.getText().toString();
+                if (checkValue.isEmpty()) {
+                    setVideoResolutionDialog(seek);
+                    mSetVideoResolution.setError(getString(R.string.global_error_field_empty));
+                    return;
+                }
+                int Value;
+                try {
+                    Value = Integer.parseInt(checkValue);
+                } catch (NumberFormatException e) {
+                    Log.e("allocationSeek", e.toString());
+                    // mSetVideoResolution.setError(e.toString());
+                    setVideoResolutionDialog(seek);
+                    mSetVideoResolution.setError(requireContext().getString(R.string.setting_set_resolution_outofrange, checkValue));
+                    return;
+                }
+                if (Value < 25) {
+                    setVideoResolutionDialog(seek);
+                    mSetVideoResolution.setError(requireContext().getString(R.string.setting_set_resolution_too_small, 25));
+                    return;
+                }
+                if (Value > 1000) {
+                    setVideoResolutionDialog(seek);
+                    mSetVideoResolution.setError(requireContext().getString(R.string.setting_set_resolution_too_big, 1000));
+                    return;
+                    }
+                seek.setValue(Value);
+                })
+            .setNegativeButton(R.string.alertdialog_cancel, null)
+            .create();
+        dialog.show();
     }
 
 }
