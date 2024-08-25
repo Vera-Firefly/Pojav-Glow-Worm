1e2ed560f22558317b6545671db47734dadf61a5
Vera-Firefly <1964762970@qq.com>
2024-08-25 15:58:16 +0800

    Fix

--- scripts/getJre.sh
+++ scripts/getJre.sh
@@ -5,14 +5,14 @@ git clone https://github.com/Vera-Firefly/android-openjdk-autobuild
 
 if [ -z "$GET_JRE" ]; then
   echo "Nvironment GET_JRE is not set, default to get jre8 and jre21"
-  rm android-openjdk-autobuild/LatestJre/jre-11
-  rm android-openjdk-autobuild/LatestJre/jre-17
+  rm -r android-openjdk-autobuild/LatestJre/jre-11
+  rm -r android-openjdk-autobuild/LatestJre/jre-17
   cp -rf android-openjdk-autobuild/LatestJre/* app_pojavlauncher/src/main/assets/components/
 else
   if [ "$GET_JRE" -eq 1 ]; then
     cp -rf android-openjdk-autobuild/LatestJre/* app_pojavlauncher/src/main/assets/components/
   elif [ "$GET_JRE" -eq 2 ]; then
-    rm android-openjdk-autobuild/LatestJre/jre-11
+    rm -r android-openjdk-autobuild/LatestJre/jre-11
     cp -rf android-openjdk-autobuild/LatestJre/* app_pojav_zh/src/main/assets/components/
   fi
 fi
